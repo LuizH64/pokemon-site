9ec66f71e815e3b922c3f13c6253f88dfc777588
Luiz H <luizhenriquealvesrodrigue64@gmail.com>
2023-05-19 23:48:17 -0300

    feat: imaplement main LP

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,10 +2,12 @@ import { Route, Routes } from "react-router-dom";
 
 // Pages
 import { NotFoundPage } from "./pages/NotFound.tsx";
+import { LandingPagePage } from "./pages/LandingPage.tsx";
 
 
 const MainRoutes = () => (
     <Routes>
+        <Route path="/" element={<LandingPagePage />} />
         <Route path="*" element={<NotFoundPage />} />
     </Routes>
 );
---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,14 @@
+// Components
+import { Header } from "../components/Header";
+import { LandingPage } from "../components/LandingPage";
+
+const LandingPagePage = () => {
+    return (
+        <>
+            <Header />
+            <LandingPage />
+        </>
+    );
+};
+
+export { LandingPagePage };
